a433274d08c4eba238306c87b84da8273699e628
Diego Benitez <diego@deegles.co>
2016-04-11 18:38:20 -0700

    Check that the directory exists before creating log file.

--- src/blinky/scripts/logger.js
+++ src/blinky/scripts/logger.js
@@ -1,6 +1,7 @@
 "use strict";
 var fs = require('fs');
 var util = require('util');
+var path = require('path');
 var config = require('./config');
 var log_path = config.logDir;
 var log_name = new Date().toISOString();
@@ -8,7 +9,9 @@ var log_stdout = process.stdout;
 var log_file;
 
 try {
-    log_file = fs.createWriteStream(util.format("%s/%s.log", log_path, log_name), {flags: 'w'}); //__dirname + '/debug.log'
+    var log_file_name = util.format("%s/%s.log", log_path, log_name);
+    ensureDirectoryExistence(log_file_name);
+    log_file = fs.createWriteStream(log_file_name, {flags: 'w'}); //__dirname + '/debug.log'
 } catch (e){
     process.stderr.write("Exception creating log file write stream: " + util.inspect(e));
 }
@@ -21,3 +24,21 @@ console.log = function(d) {
     }
     log_stdout.write(util.format(d) + '\n');
 };
+
+function ensureDirectoryExistence(filePath) {
+    var dirname = path.dirname(filePath);
+    if (directoryExists(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
+
+function directoryExists(path) {
+    try {
+        return fs.statSync(path).isDirectory();
+    }
+    catch (err) {
+        return false;
+    }
+}
